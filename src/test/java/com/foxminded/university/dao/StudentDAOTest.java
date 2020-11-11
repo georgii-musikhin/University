dc028f21fbc8012c9288547aab271725fcf36b97
Georgii Musikhin <sadjesty@gmail.com>
2020-11-11 15:29:36 +0300

    Start adding tests for dao classes

--- src/test/java/com/foxminded/university/dao/StudentDAOTest.java
+++ src/test/java/com/foxminded/university/dao/StudentDAOTest.java
@@ -1,43 +1,26 @@
 package com.foxminded.university.dao;
 
 import com.foxminded.university.domain.Student;
-import org.dbunit.DataSourceBasedDBTestCase;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 
-class StudentDAOTest extends DataSourceBasedDBTestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StudentDAOTest {
     private final StudentDAO studentDAO = new StudentDAO();
-
-    @Override
-    protected DataSource getDataSource() {
-        JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setURL(
-                "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1;init=runscript from 'classpath:schema.sql'");
-        dataSource.setUser("sa");
-        dataSource.setPassword("sa");
-        return dataSource;
-    }
-
-    @Override
-    protected IDataSet getDataSet() throws Exception {
-        return new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
-                .getResourceAsStream("dbUnitData.xml"));
-    }
 
     @Test
     void addNewStudentToBase() throws DAOException {
-        Student expected = new Student(2, "Loh", "Lohov");
+        Student expected = new Student(3, "Max", "Ushakov");
+        Student actual = studentDAO.addNewStudentToBase("Max", "Ushakov");
 
-        Student actual = studentDAO.addNewStudentToBase("Loh", "Lohov");
         assertEquals(expected, actual);
     }
 
     @Test
     void setStudentToGroup() {
+        Student expected = new Student(3, "Max", "Ushakov");
     }
 
     @Test
---
+++ src/test/resources/fillTables.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (first_name, last_name)  VALUES
+('Mikhail', 'Bobrov'),
+('Gera', 'Bodrov');
